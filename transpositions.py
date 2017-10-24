ac7abc34df0420724d942bd0874ce62d0bb5c921
blamedcloud <denverwoodward@yahoo.com>
2017-10-23 23:20:08 -0400

    added idea for transposition_manager

---
+++ transpositions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#alphabeta.py
+
+#class to essentially memoize the search function of ABPruning_Tree.
+#basically list of tuples of (Game_State, depth, alpha, beta, value, best_child_depth)
+#implement __contains__ function.
+
+class Transposition_Manager(object):
+	pass
+	#TO DO
